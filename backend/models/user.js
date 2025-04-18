3962db9405caf46308b0fa6e07e8c42609354d48
Mikael Malania <m.malania@noxtton.com>
2025-04-18 22:11:23 +0400

    fix: removed version keys and included timestamps

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+// Schema was not containing email field for now but index was still left in db
+// Which was causing an issue so I dropped a unnesseary index in db from mongo shell
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
+}, {versionKey: false, timestamps: true});
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
